32ae77f4580645bfb9e965f3e0202c00b13decf3
Romez1990 <Romez1990@gmail.com>
2025-11-30 05:03:29 +0300

    feat(config): modify config

--- config.py
+++ config.py
@@ -3,8 +3,12 @@ from datetime import timedelta
 from data import RetentionPolicy
 
 backup_retention_policy = [
-    RetentionPolicy(interval=timedelta(minutes=15), keep_count=3),
-    RetentionPolicy(interval=timedelta(hours=1), keep_count=6),
-    RetentionPolicy(interval=timedelta(days=1), keep_count=5),
+    RetentionPolicy(interval=timedelta(minutes=15), keep_count=2),
+    RetentionPolicy(interval=timedelta(minutes=30), keep_count=2),
+    RetentionPolicy(interval=timedelta(hours=1), keep_count=2),
+    RetentionPolicy(interval=timedelta(hours=2), keep_count=2),
+    RetentionPolicy(interval=timedelta(hours=6), keep_count=2),
+    RetentionPolicy(interval=timedelta(days=1), keep_count=3),
+    RetentionPolicy(interval=timedelta(days=3), keep_count=2),
     RetentionPolicy(interval=timedelta(weeks=1), keep_count=4),
 ]
